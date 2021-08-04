88855064f6f3594c171104698a11e4732b17acda
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-08-04 15:09:18 -0400

    fixed app not rending error

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-document.addEventListener('DomContentLoaded', () => {
-    const grid = document.querySelector('grid')
+document.addEventListener('DOMContentLoaded', () =>{
+    const grid = document.querySelector('.grid')
     const width = 8
     const squares = []
-
     const candyColors = [
         'red',
         'yellow',
         'orange',
         'purple',
         'green',
-        'blue'
     ]
-    //Create Board 
+    //Create Board
     function createBoard() {
-        for (let i =0; i < width*width; i++) {
+        for (let i=0; i < width*width; i++) {
             const square = document.createElement('div')
-            let randomColor = (Math.random() *candyColors.length)
+            square.setAttribute('draggable', true)
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            square.style.backgroundColor = candyColors[randomColor]
             grid.appendChild(square)
             squares.push(square)
         }
     }
     createBoard()
-})
+ })
