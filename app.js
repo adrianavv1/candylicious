5fb92e0b5398b9020fafa0bd1cf76d840b62923e
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-08-04 14:40:27 -0400

    adding candy colors

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ document.addEventListener('DomContentLoaded', () => {
     const width = 8
     const squares = []
 
+    const candyColors = [
+        'red',
+        'yellow',
+        'orange',
+        'purple',
+        'green',
+        'blue'
+    ]
     //Create Board 
     function createBoard() {
         for (let i =0; i < width*width; i++) {
             const square = document.createElement('div')
+            let randomColor = (Math.random() *candyColors.length)
             grid.appendChild(square)
             squares.push(square)
         }
