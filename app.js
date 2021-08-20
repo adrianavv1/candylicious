aa65e13c52c3a939b3b659073dd67b10c8fcb471
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-08-20 13:20:43 -0400

    added function to check for row of four

--- app.js
+++ app.js
@@ -85,12 +85,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   //Checking for matches
-  //check for row of three
-  function checkRowForThree() {
-    for (i = 0; i < 61; i++) {
-      let rowOfThree = [i, i + 1, i + 2];
+  //check for row of four
+  function checkRowForFour() {
+    for (i = 0; i < 60; i++) {
+      let rowOfFour = [i, i + 1, i + 2, i + 3];
       let decideColor = squares[i].style.backgroundColor;
       const isBlank = squares[i].style.backgroundColor === "";
+
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+      if (notValid.includes(i)) continue
 
       if (
         rowOfThree.every(
@@ -106,6 +109,54 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   checkRowForThree()
+  
+  //check for column of three
+  function checkColumnForThree() {
+    for (i = 0; i < 47; i++) {
+      let columnOfThree = [i, i +width, i + width*2];
+      let decideColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === "";
+
+      if (
+        columnOfThree.every(
+          (index) =>
+            squares[index].style.backgroundColor === decideColor && !isBlank
+        )
+      ) {
+        score += 3;
+        columnOfThree.forEach((index) => {
+          squares[index].style.backgroundColor = "";
+        });
+      }
+    }
+  }
+  checkColumnForThree()
+
+  //check for row of three
+  function checkRowForThree() {
+    for (i = 0; i < 61; i++) {
+      let rowOfThree = [i, i + 1, i + 2];
+      let decideColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === "";
+
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+      if (notValid.includes(i)) continue
+
+      if (
+        rowOfThree.every(
+          (index) =>
+            squares[index].style.backgroundColor === decideColor && !isBlank
+        )
+      ) {
+        score += 3;
+        rowOfThree.forEach((index) => {
+          squares[index].style.backgroundColor = "";
+        });
+      }
+    }
+  }
+  checkRowForThree()
+
   //check for column of three
   function checkColumnForThree() {
     for (i = 0; i < 47; i++) {
